83fa4c8b3e835c8a8160641c6246994b8fe2ab6a
Tom de Geus <tdegeus@users.noreply.github.com>
2024-03-11 15:01:31 +0000

    CI: minor updates (#117)

--- docs/conf.py
+++ docs/conf.py
@@ -3,15 +3,12 @@ import sys
 
 sys.path.insert(0, os.path.abspath(".."))
 
-# Project information
-
 project = "texplain"
 copyright = "2017-2023, Tom de Geus"
 author = "Tom de Geus"
 html_theme = "furo"
 autodoc_type_aliases = {"Iterable": "Iterable", "ArrayLike": "ArrayLike"}
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
